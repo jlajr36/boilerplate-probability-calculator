f304f171ad14645ccc14254182c73960d98e263a
jlajr36 <jlajr36@gmail.com>
2021-05-13 18:29:37 -0500

    Passed
    Passed all test.

--- prob_calculator.py
+++ prob_calculator.py
@@ -11,10 +11,22 @@ class Hat:
     def draw(self, numNeed):
         listOut = []
         for cnt in range(numNeed):
-            randItem = random.choice(self.contents)
-            listOut.append(randItem)
-            self.contents.remove(randItem)
+            try:
+                randItem = random.choice(self.contents)
+                listOut.append(randItem)
+                self.contents.remove(randItem)
+            except:
+                pass
         return listOut
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    total = 0
+    for num in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn = hat_copy.draw(num_balls_drawn)
+        for count in expected_balls:
+            if expected_balls[count] > drawn.count(count):
+                total -= 1
+                break
+        total += 1
+    return total/num_experiments
